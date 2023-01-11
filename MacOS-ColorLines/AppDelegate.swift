0467df4b1627aa8f4fd684e8c7218a0f3a316f23
KhimuninAA <khimuninaa@gmail.com>
2023-01-11 10:41:34 +0300

    Add menu

--- MacOS-ColorLines/AppDelegate.swift
+++ MacOS-ColorLines/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         rootView = RootView()
         window.contentView = rootView
         window.delegate = self
+        self.window.title = "ColorLines3D"
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -34,6 +35,34 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool{
         return true
+    }
+    
+    private var preferenceWindow: NSWindow?
+    private func openInWindow(view: NSView, rect: CGRect, title: String, isShowBar: Bool){
+        if preferenceWindow != nil {
+            preferenceWindow?.orderOut(self)
+            preferenceWindow = nil
+        }
+        
+        preferenceWindow = NSWindow(contentRect: rect, styleMask: [.closable, .titled, .unifiedTitleAndToolbar], backing: .buffered, defer: false)
+        if isShowBar{
+            preferenceWindow?.titlebarAppearsTransparent = true
+            preferenceWindow?.titleVisibility = .hidden
+        }
+        preferenceWindow?.delegate = self
+        preferenceWindow?.title = title
+        preferenceWindow?.contentView = view
+        preferenceWindow?.contentView?.frame = CGRect(x: 0, y: 0, width: rect.width, height: rect.height)
+    }
+    
+    @IBAction func showAbout(_ sender: Any) {
+        if let mainSize = NSScreen.main?.frame.size{
+            let aboutSize = CGSize(width: 400, height: 200)
+            let aboutFrame = CGRect(x: 0.5 * (mainSize.width - aboutSize.width), y: 0.5 * (mainSize.height - aboutSize.height), width: aboutSize.width, height: aboutSize.height)
+            let aboutView = AboutView(frame: aboutFrame)
+            openInWindow(view: aboutView, rect: aboutFrame, title: "О программе", isShowBar: true)
+            preferenceWindow?.makeKeyAndOrderFront(self)
+        }
     }
 }
 
